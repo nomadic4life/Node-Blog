11f987f81e0e3d5d540909d36076d04a5faa38bf
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-09 14:18:31 -0800

    all user end points created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const postDB = require('./data/helpers/postDb.js');
 const userDB = require('./data/helpers/userDb.js');
 const cors = require('cors');
+const morgan = require('morgan');
 const knex = require('knex');
 
 const userNamesCapitalize = (req, res, next) => {
@@ -9,37 +10,51 @@ const userNamesCapitalize = (req, res, next) => {
   userDB.get(id)
   .then(info => {
     if(info) {
-      console.log(id, info.name)
-
         req.info = {...info, name: info.name.toUpperCase()};
         next();
       }
       else res.status(404).json({message: "post not found with specified ID"})
 
     })
-    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+    .catch(err => res.status(500).json({errorMessage: "request failed"}))
 }
 
 const server = express();
+server.get(morgan('tiny'));
 server.use(cors())
 server.use(express.json());
 
+
+server.get('/users', (req, res) => {
+
+  userDB.get()
+  .then(info => {
+    res.status(200).json(info);
+  })
+  .catch(err => res.status(500).json({errorMessage: "request failed"}))
+})
+
 server.get('/posts', (req, res) => {
+  
   postDB.get()
     .then(info => {
       res.status(200).json(info);
-
     })
-    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+    .catch(err => res.status(500).json({errorMessage: "request failed"}))
 })
 
-server.get('/users', (req, res) => {
-  userDB.get()
-    .then(info => {
-      res.status(200).json(info);
+server.get('/users/:id/posts', userNamesCapitalize, (req, res) => {
+  const {id} = req.params;
+  userDB.getUserPosts(id)
+  .then(post => {
+    console.log(id, post)
+    res.status(200).json(post)
+  })
+  .catch(err => res.status(500).json({errorMessage: "failed to retrive post for user"}))
+})
 
-    })
-    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+server.get('/users/:id', userNamesCapitalize, (req, res) => {
+  if(req.info) res.status(200).json(req.info);
 })
 
 server.get('/posts/:id', (req, res) => {
@@ -47,18 +62,31 @@ server.get('/posts/:id', (req, res) => {
   postDB.get(id)
     .then(info => {
       if(info) res.status(200).json(info);
-      else res.status(404).json({message: "user not found with specified ID"})
+      else res.status(404).json({message: "post not found with specified ID"})
 
     })
-    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+    .catch(err => res.status(500).json({errorMessage: "request failed"}))
   
 })
 
-server.get('/users/:id', userNamesCapitalize, (req, res) => {
-  const {id} = req.params;
-  if(req.info) res.status(200).json(req.info);
-})
 
+server.post('/users', (req, res) => {
+
+  const {name} = req.body
+  if(!name && typeof name !== 'string') {
+    res.status(400).json({errorMessage: "invalid name, name needs to be a string"})
+  }
+
+  if(name.length > 128) {
+    res.status(406).json({errorMessage: "name more than 128 characters"})
+  }
+
+  userDB.insert({name})
+  .then(id => {
+    res.status(201).json({name: name,id: id.id});
+  })
+  .catch(err => res.status(500).json({errorMessage: "failed to create post"}))
+})
 
 server.post('/posts', (req, res) => {
   postDB.insert(req.post)
@@ -69,15 +97,31 @@ server.post('/posts', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
 })
 
-server.post('/users', (req, res) => {
-  userDB.insert(req.user)
-    .then(id => {
-      res.status(201).json(id);
 
-    })
-    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+server.put('/users/:id', (req, res) => {
+  
+  const {id} = req.params;
+  const {name} = req.body;
+  
+  if(!name && typeof name !== 'string') {
+    res.status(400).json({errorMessage: "invalid name, name needs to be a string"})
+  }
+
+  if(name.length > 128) {
+    res.status(406).json({errorMessage: "name more than 128 characters"})
+  }
+
+  
+  userDB.update(id, {name})
+  .then(count => {
+    if(count) {
+      res.status(200).json({id,name});
+    } else {
+      res.status(404).json({errorMessage: "failed to make update on specified ID"})
+    }
+  })
+  .catch(err => res.status(500).json({errorMessage: "failed request"}))
 })
-
 
 server.put('/posts/:id', (req, res) => {
   postDB.update(id, req.post)
@@ -88,14 +132,35 @@ server.put('/posts/:id', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
 })
 
-server.put('/users/:id', (req, res) => {
-  userDB.update(id, req.user)
-    .then(id => {
-      res.status(201).json(id);
 
-    })
-    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+server.delete('/users/:id', (req, res) => {
+  
+  const {id} = req.params;
+
+  userDB.remove(id)
+  .then(count => {
+    if(count) {
+      res.status(204).end()
+    } else {
+      res.status(404).json({errorMessage: "failed to make delete request on specified ID"})
+    }
+  })
+  .catch(err => res.status(500).json({errorMessage: "failed request"}))
 })
 
+server.delete('/posts/:id', (req, res) => {
+  
+  const {id} = req.params;
+
+  postDB.remove(id)
+  .then(count => {
+    if(count) {
+      res.status(204).end()
+    } else {
+      res.status(404).json({errorMessage: "failed to make delete request on specified ID"})
+    }
+  })
+  .catch(err => res.status(500).json({errorMessage: "failed request"}))
+})
 
 server.listen('8000', () => console.log('server live on port 8000'));
