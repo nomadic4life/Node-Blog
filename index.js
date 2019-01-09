0b5be2f407685f067077f0b2292188db6c299102
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-09 05:46:34 -0800

    database connected

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const postDB = require('./data/helpers/postDb.js');
+const userDB = require('./data/helpers/userDb.js');
 const cors = require('cors');
 const knex = require('knex');
 
 const server = express();
 server.use(cors())
 server.use(express.json());
+
+server.get('/posts', (req, res) => {
+  postDB.get()
+    .then(info => {
+      res.status(200).json(info);
+
+    })
+    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+})
+
+server.get('/users', (req, res) => {
+  userDB.get()
+    .then(info => {
+      res.status(200).json(info);
+
+    })
+    .catch(err => res.status(500).json({errorMessage: "could get make get request to db"}))
+})
 
 
 server.listen('8000', () => console.log('server live on port 8000'));
