82f914faa392f0d562b24d5e50e5d83b67cfecf1
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-09 14:46:05 -0800

    fixed some bugs with post end point for users

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const morgan = require('morgan');
 const knex = require('knex');
 
 const userNamesCapitalize = (req, res, next) => {
-  const {id} = req.params;
-  userDB.get(id)
-  .then(info => {
-    if(info) {
-        req.info = {...info, name: info.name.toUpperCase()};
-        next();
-      }
-      else res.status(404).json({message: "post not found with specified ID"})
+  const {name} = req.body;
+  if(!name && typeof name !== 'string') {
+    res.status(400).json({errorMessage: "invalid name, name needs to be a string"})
+  }
 
-    })
-    .catch(err => res.status(500).json({errorMessage: "request failed"}))
+  if(name.length > 128) {
+    res.status(406).json({errorMessage: "name more than 128 characters"})
+  }
+
+  req.userName = name.toUpperCase();
+  console.log(req.userName, name)
+  next();
 }
 
 const server = express();
@@ -43,18 +44,23 @@ server.get('/posts', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: "request failed"}))
 })
 
-server.get('/users/:id/posts', userNamesCapitalize, (req, res) => {
+server.get('/users/:id/posts', (req, res) => {
   const {id} = req.params;
   userDB.getUserPosts(id)
   .then(post => {
-    console.log(id, post)
     res.status(200).json(post)
   })
   .catch(err => res.status(500).json({errorMessage: "failed to retrive post for user"}))
 })
 
-server.get('/users/:id', userNamesCapitalize, (req, res) => {
-  if(req.info) res.status(200).json(req.info);
+server.get('/users/:id', (req, res) => {
+  const {id} = req.params;
+  userDB.get(id)
+  .then(info => {
+    if(info) res.status(200).json(info);
+    else res.status(404).json({message: "post not found with specified ID"})
+  })
+  .catch(err => res.status(500).json({errorMessage: "request failed"}))
 })
 
 server.get('/posts/:id', (req, res) => {
@@ -70,20 +76,13 @@ server.get('/posts/:id', (req, res) => {
 })
 
 
-server.post('/users', (req, res) => {
+server.post('/users', userNamesCapitalize, (req, res) => {
 
-  const {name} = req.body
-  if(!name && typeof name !== 'string') {
-    res.status(400).json({errorMessage: "invalid name, name needs to be a string"})
-  }
-
-  if(name.length > 128) {
-    res.status(406).json({errorMessage: "name more than 128 characters"})
-  }
+  const {userName : name} = req;
 
   userDB.insert({name})
-  .then(id => {
-    res.status(201).json({name: name,id: id.id});
+  .then(userid => {
+    res.status(201).json({name: name, id: userid.id});
   })
   .catch(err => res.status(500).json({errorMessage: "failed to create post"}))
 })
@@ -98,20 +97,10 @@ server.post('/posts', (req, res) => {
 })
 
 
-server.put('/users/:id', (req, res) => {
-  
+server.put('/users/:id', userNamesCapitalize, (req, res) => {
+  console.log('put');
   const {id} = req.params;
-  const {name} = req.body;
-  
-  if(!name && typeof name !== 'string') {
-    res.status(400).json({errorMessage: "invalid name, name needs to be a string"})
-  }
-
-  if(name.length > 128) {
-    res.status(406).json({errorMessage: "name more than 128 characters"})
-  }
-
-  
+  const {name} = req.userName;
   userDB.update(id, {name})
   .then(count => {
     if(count) {
